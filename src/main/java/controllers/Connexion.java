9d2a136316383a2e4f7c99f3e6692516389f7928
Raphael Mezui <raphael.mezui@icloud.com>
2022-02-15 12:39:19 +0100

    connexion validée

--- src/main/java/controllers/Connexion.java
+++ src/main/java/controllers/Connexion.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bll.ConnecterManager;
 
@@ -53,7 +54,17 @@ public class Connexion extends HttpServlet {
 			mdp = request.getParameter("password").trim();
 		}
 		
-		connexionMng.selectUtilisateur(login, mdp);
+		int identifiant = connexionMng.selectUtilisateur(login, mdp);
+		if(identifiant != 0) {
+			HttpSession session = request.getSession();
+			session.setAttribute("utilisateur", identifiant);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/Accueil.jsp");
+			if (rd != null) {
+				rd.forward(request, response);
+			}
+		}else {
+			System.out.println("Utilisateur inconnu");
+		}
 	}
 
 }
