c4e5f60f27261e429456fbef705a021898a8cfe9
raphael potet <raphael.potet2021@campus-eni.fr>
2022-02-16 17:02:39 +0100

    servlet jdbc et manager Modification Profil

---
+++ src/main/java/bll/ModificationProfilManager.java
@@ -0,0 +1,22 @@
+package bll;
+import bo.Utilisateur;
+import dal.ModificationProfilJdbc;
+public class ModificationProfilManager {
+	private ModificationProfilJdbc modificationProfilMng;
+	
+	
+	public ModificationProfilManager(){
+		super();
+		modificationProfilMng= new ModificationProfilJdbc();
+		}
+	
+	
+	public int update(Utilisateur modificationUtilisateur) {
+		return modificationProfilMng.update(modificationUtilisateur);
+		
+	}
+
+	
+	
+	
+}
