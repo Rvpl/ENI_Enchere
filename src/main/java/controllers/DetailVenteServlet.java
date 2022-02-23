631b251946c6051027be5500d7c33586ceffc4e0
raphael potet <raphael.potet2021@campus-eni.fr>
2022-02-23 11:22:17 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/Rvpl/ENI_Enchere.git into main

--- src/main/java/controllers/DetailVenteServlet.java
+++ src/main/java/controllers/DetailVenteServlet.java
@@ -1,11 +1,7 @@
 package controllers;
 
-
-
 import java.io.IOException;
 import java.sql.SQLException;
-
-
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,96 +10,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 import bll.DetailVenteManager;
 import bo.Article;
 
 /**
-* Servlet implementation class EnchereServlet
-*/
+ * Servlet implementation class EnchereServlet
+ */
 @WebServlet("/detailVente")
 public class DetailVenteServlet extends HttpServlet {
-private static final long serialVersionUID = 1L;
-private DetailVenteManager detailVenteMng;
+	private static final long serialVersionUID = 1L;
+	private DetailVenteManager detailVenteMng;
 
-
-
-/**
-* @see HttpServlet#HttpServlet()
-*/
-public DetailVenteServlet() {
-super();
-detailVenteMng = new DetailVenteManager();
-
-
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DetailVenteServlet() {
+		super();
+		detailVenteMng = new DetailVenteManager();
 
 // TODO Auto-generated constructor stub
-}
+	}
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+  
+		try {
+			if (detailVenteMng.selectAll(Integer.parseInt(request.getParameter("noArticle"))) != null) {
+				Article article = detailVenteMng.selectAll(Integer.parseInt(request.getParameter("noArticle")));
+				request.setAttribute("nomArticle", article.getNomArticle());
+				request.setAttribute("description", article.getDescription());
+				request.setAttribute("categorie", article.getNoCategorie().getLibelle());
+				request.setAttribute("prixVente", article.getPrixVente());
+				request.setAttribute("miseAPrixStr", article.getMiseAPrix());
+				request.setAttribute("dateFinEncheresStr", article.getDateFinEncheres());
+				request.setAttribute("rue", article.getRetrait().getRue());
+				request.setAttribute("codePostal", article.getRetrait().getCodePostal());
+				request.setAttribute("ville", article.getRetrait().getVille());
+				request.setAttribute("pseudo", article.getUtilisateur().getPseudo());
 
-
-/**
-* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-*/
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-int numeroUtil=0;
-int prixInitial=0;
-int prixVente= 0;
-int dateFinEncheres= 0;
-int dateDebutncheres= 0;
-String categorie= null;
-String prixVenteStr= null;
-String description= null;
-String dateFinEncheresStr= null;
-String dateDebutncheresStr= null;
-String nomArticle= null;
-String idArticle= null;
-String ville= null;
-String rue= null;
-String codePostal= null;
-String pseudo = null;
-String nom= null;
-String prenom= null;
-try {
-if(detailVenteMng.selectAll(Integer.parseInt(request.getParameter("noArticle")))!= null) {
-Article article= detailVenteMng.selectAll(Integer.parseInt(request.getParameter("noArticle")));
-request.setAttribute("nomArticle", article.getNomArticle());
-request.setAttribute("description", article.getDescription());
-request.setAttribute("categorie", article.getNoCategorie().getLibelle());
-request.setAttribute("prixVente", article.getPrixVente());
-request.setAttribute("miseAPrixStr", article.getMiseAPrix());
-request.setAttribute("dateFinEncheresStr", article.getDateFinEncheres());
-request.setAttribute("rue", article.getRetrait().getRue());
-request.setAttribute("codePostal", article.getRetrait().getCodePostal());
-request.setAttribute("ville", article.getRetrait().getVille());
-request.setAttribute("pseudo", article.getUtilisateur().getPseudo());
-
-
-}
-} catch (NumberFormatException e) {
+			}
+		} catch (NumberFormatException e) {
 // TODO Auto-generated catch block
-e.printStackTrace();
-} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
 // TODO Auto-generated catch block
-e.printStackTrace();
-}
-request.getRequestDispatcher("/WEB-INF/JSP/DetailVente.jsp").forward(request, response);
-RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/DetailVente.jsp");
-}
+			e.printStackTrace();
+		}
+		request.getRequestDispatcher("/WEB-INF/JSP/DetailVente.jsp").forward(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/DetailVente.jsp");
+	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-
-/**
-* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-*/
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-
-
-}
-
-
+	}
 
 }
